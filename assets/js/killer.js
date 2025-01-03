5d607138a0004443073576d708107703d5fa4cde
corsinmatt80 <corsin.matt@student.unisg.ch>
2025-01-03 22:50:25 +0100

    trial v3

--- assets/js/killer.js
+++ assets/js/killer.js
@@ -11,33 +11,75 @@ function initializePlayers() {
 
     players = JSON.parse(storedPlayers).map(name => ({
         name,
-        lives: 3,
-        killer: false
+        hits: 0,
+        killer: false,
+        randomNumber: Math.floor(Math.random() * 20) + 1
     }));
 
-    // Aktuellen Spieler anzeigen
     updateCurrentPlayer();
 }
 
-// Aktuellen Spieler anzeigen
 function updateCurrentPlayer() {
     const currentPlayer = players[currentPlayerIndex];
-    document.getElementById("current-player-name").innerText = currentPlayer.name;
+    document.getElementById("current-player-name").innerText = `${currentPlayer.name} : ${currentPlayer.randomNumber}`;
 }
 
-// Treffer-Handling
 function handleHit(segment) {
     const currentPlayer = players[currentPlayerIndex];
+    const hitNumber = parseInt(segment.split(' ')[1], 10);
 
-    // Beispiel: Trefferlogik
     if (currentPlayer.killer) {
-        // Wenn der Spieler ein Killer ist, kann er andere Spieler angreifen
-        console.log(`${currentPlayer.name} hit ${segment}`);
+        for(let i = 0; i < players.length;i++){
+            if(hitNumber === players[i].randomNumber && currentPlayer.name !== players[i].name){
+                if(segment.startsWith('3x')){
+                    players[i].hits = players[i].hits - 3;
+                }
+                if(segment.startsWith('2x')) {
+                    players[i].hits = players[i].hits - 2;
+                }
+                if(segment.startsWith('1x')) {
+                    players[i].hits = players[i].hits - 1;
+                }
+                if(players[i].hits < 0){
+                    players.splice(i,1);
+                }
+            }
+        }
+
     } else {
-        // Wenn der Spieler kein Killer ist, trifft er auf seine eigenen Segmente
-        console.log(`${currentPlayer.name} scored on ${segment}`);
+        if (segment.startsWith('3x')) {
+            const hitNumber = parseInt(segment.split(' ')[1], 10);
+            if (hitNumber === currentPlayer.randomNumber) {
+                currentPlayer.hits = 3;
+                currentPlayer.killer = true;
+                console.log(`Hits of ${currentPlayer.name} : ${currentPlayer.hits}`);
+            }
+        }
+        if (segment.startsWith('2x')) {
+            if (hitNumber === currentPlayer.randomNumber) {
+                currentPlayer.hits = currentPlayer.hits + 2;
+                if (currentPlayer.hits > 3) {
+                    currentPlayer.hits = 3;
+                    currentPlayer.killer = true;
+                }
+                console.log(`Hits of ${currentPlayer.name} : ${currentPlayer.hits}`);
+            }
+        }
+        if (segment.startsWith('1x')) {
+            if (hitNumber === currentPlayer.randomNumber) {
+                currentPlayer.hits = currentPlayer.hits + 2;
+                if (currentPlayer.hits < 3) {
+                    currentPlayer.hits = currentPlayer.hits + 1;
+                }
+                if(currentPlayer.hits === 3){
+                    currentPlayer.killer = true;
+                }
+                console.log(`Hits of ${currentPlayer.name} : ${currentPlayer.hits}`);
+            }
+        }
     }
 }
+
 
 // Zug beenden
 function endTurn() {
