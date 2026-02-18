ff5b9d328eb4fb60a930fec9b53094d553901513
corsinmatt80 <corsin.matt@adon.li>
2026-02-18 20:11:53 +0100

    configs

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,21 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/Dart/',
+    base: './',
     plugins: [react()],
     server: {
         port: 5173,
         open: true
+    },
+    build: {
+        outDir: 'docs',
+        assetsDir: 'assets',
+        rollupOptions: {
+            output: {
+                entryFileNames: 'assets/[name]-[hash].js',
+                chunkFileNames: 'assets/[name]-[hash].js',
+                assetFileNames: 'assets/[name]-[hash].[ext]'
+            }
+        }
     }
 });
